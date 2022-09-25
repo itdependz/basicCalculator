840cb1e9e2d64cea64ba7e100c072ea2afb81748
itdependz <93829749+itdependz@users.noreply.github.com>
2022-09-25 12:03:58 -0400

    here is a test commit

---
+++ websiteFishing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class websiteFishing
+{
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+
+    for(int i = 0; i<6; i++){
+      
+    }
+  }
+}
